508ee76194aa5ed7d9ff3688b6afc874ec01b3e4
Tiphaine Dumur <tidumur@gmail.com>
2020-10-10 17:41:02 +0200

    add ISBN request to the requestQueue

--- app/src/main/java/fr/caubin/books/books/BookManager.java
+++ app/src/main/java/fr/caubin/books/books/BookManager.java
@@ -1,22 +1,66 @@
 package fr.caubin.books.books;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import android.app.Application;
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import fr.caubin.books.utils.NetworkManager;
 
 /**
  * Connect to the book API
  */
-public class BookManager {
-    URL bookAPIUrl;
+public class BookManager{
+    Context applicationContext;
+    // TODO : set "https://openlibrary.org/" to be a project constant
+    String bookAPIUrl = "https://openlibrary.org/" + "books/" ;
+    RequestQueue requestQueue;
 
-    public Book getBookFromISBN(String isbn) throws MalformedURLException {
+    public BookManager(Context applicationContext) {
+        this.applicationContext = applicationContext;
+        requestQueue = NetworkManager.getInstance(applicationContext).getRequestQueue();
+    }
+
+    public void getBookFromISBN(String isbn)
+    {
         Book book = new Book();
-        bookAPIUrl = new URL("https://openlibrary.org/books/" + isbn);
+        String  bookUrl = bookAPIUrl + isbn + ".json";
 
-        return book;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, bookUrl, null, new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Book book = new Book();
+                        try {
+                            //TODO : use complementary manager to set authors
+                            book.setAuthor(response.getString("authors"));
+
+                            book.setTitle(response.getString("title"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //TODO : handle the error
+                        Book book = new Book();
+                        String test = error.getMessage();
+                    }
+                });
+        NetworkManager.getInstance(applicationContext).addToQueueRequest(jsonObjectRequest);
+
+
+
     }
 }
